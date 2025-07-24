4c4bb8e80c27d292691f5141c02974e256556fe9
popendus228 <156169274+popendus228@users.noreply.github.com>
2025-07-25 01:53:48 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const urlsToCache = [
   '/popendus228.github.io/',
   '/gritskevich.github.io/index.html',
   'manifest.json',
+  'favicon.ico',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
